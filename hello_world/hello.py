7ba2b1ed263a71ffc20114cdb2a1167ee5cd860d
Zhl1102 <1533938229@qq.com>
2022-03-26 10:19:54 +0800

    first pip

---
+++ hello_world/hello.py
@@ -0,0 +1,7 @@
+def hello_world(str1, str2):
+	str = f"{str1} {str2}!"
+	return str.title()
+
+str1 = "hello"
+str2 = "world"
+print(hello_world(str1, str2))
--- setup.py
+++ setup.py
@@ -5,19 +5,18 @@ from distutils.core import setup
 import setuptools
 
 setup(
-    name='hello-world', # 包的名字
+    name='hello_world', # 包的名字
     version='0.0.1',  # 版本号
     description='project describe',  # 描述
     author='watermelon', # 作者
     author_email='1533938229@qq.com',  # 你的邮箱**
     url='https://github.com/Zhl1102',  # 可以写github上的地址，或者其他地址
-    packages=setuptools.find_packages(exclude=['test', 'examples', 'script', 'tutorials']),  # 包内不需要引用的文件夹
+    packages=setuptools.find_packages(),  # 包内需要引用的文件夹
     
     # 依赖包
     install_requires=[
         'numpy',
         'torchvision',
-        'flask',
-        'hello-world'
+        'flask'
     ]
 )
